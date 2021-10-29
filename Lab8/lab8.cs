f1d6fcc468bfda1d7df3bd459f187a8207743fee
Damirio <damirnizamov7@mail.ru>
2021-10-29 13:57:58 +0300

    made 8 lab (100%)

--- Lab8/lab8.cs
+++ Lab8/lab8.cs
@@ -70,7 +70,7 @@ namespace Lab8
         public static Set operator +(Set sample1, Set sample2)  // DONE!
         {
             int[] buf = { };
-            Set buf_set = new Set();
+            Set buf_set = new Set(buf);
             for (int i = 0; i < sample2.Count; i++) {
                 buf_set.Add(sample1[i]);
             }
@@ -117,6 +117,7 @@ namespace Lab8
                     buf_set.Add(sample1[i]);
                     flag = true;
                 }
+                else { flag = true; }
             }
             return buf_set;
         }
@@ -167,27 +168,86 @@ namespace Lab8
 
             Set sample1 = new Set(s1);
             Set sample2 = new Set(s2);
+            Console.WriteLine();
+            Console.Write("Set 1: ");
             sample1.ShowSet();
+            Console.Write("Set 2: ");
             sample2.ShowSet();
-            sample1.Add(3);
-            Console.WriteLine("ADD");
-            sample1.ShowSet();
-            Console.WriteLine($"Index of '1' = {sample1.IndexOf(1)}");
-            Console.WriteLine("Operetor '+'");
-            Set sample3 = sample1 + sample2;
-            sample3.ShowSet();
-            Console.WriteLine("Operetor '++'");
-            
-            sample1++;
-            sample1.ShowSet();
-            Console.WriteLine("Operetor '*'"); // DON't WORK!
-            Set sample4 = sample1 * sample2;
-            sample4.ShowSet();
-            Console.WriteLine("Operetor '/'"); // DON'T WORK!
-            Set sample5 = sample1 / sample2;
-            sample5.ShowSet();
-            Console.WriteLine("Operetor '>'"); // DON'T WORK!
-            Console.WriteLine(sample1 > sample2);
+            Console.WriteLine();
+            // Add()
+            {
+                Console.WriteLine("Demonstration of method Add()");
+                sample1.Add(3);
+                sample1.ShowSet();
+                Console.WriteLine();
+            }
+            //---
+            //IndexOf()
+            {
+                Console.WriteLine("Demonstration of method IndexOf()");
+                Console.WriteLine($"Index of '1' = {sample1.IndexOf(1)}");
+                Console.WriteLine();
+            }
+            //---
+            // +
+            {
+                Console.WriteLine("Operetor '+'");
+                Console.Write("Set 1: ");
+                sample1.ShowSet();
+                Console.Write("Set 2: ");
+                sample2.ShowSet();
+                Set sample3 = sample1 + sample2;
+                Console.Write("Result: ");
+                sample3.ShowSet();
+                Console.WriteLine();
+            }
+            // ++
+            {
+                Console.WriteLine("Operetor '++'");
+                Console.Write("Set: ");
+                sample1.ShowSet();
+                sample1++;
+                Console.Write("Result: ");
+                sample1.ShowSet();
+                Console.WriteLine();
+            }
+            //---
+            // * 
+            {
+                Console.WriteLine("Operetor '*'");
+                Console.Write("Set 1: ");
+                sample1.ShowSet();
+                Console.Write("Set 2: ");
+                sample2.ShowSet();
+                Set sample4 = sample1 * sample2;
+                Console.Write("Result: ");
+                sample4.ShowSet();
+                Console.WriteLine();
+            }
+            //---
+            {
+                Console.WriteLine("Operetor '/'");
+                Console.Write("Set 1: ");
+                sample1.ShowSet();
+                Console.Write("Set 2: ");
+                sample2.ShowSet();
+                Set sample5 = sample1 / sample2;
+                Console.Write("Result Set1 / Set2: ");
+                sample5.ShowSet();
+                Set sample6 = sample2 / sample1;
+                Console.Write("Result Set2 / Set1: ");
+                sample6.ShowSet();
+                Console.WriteLine();
+            }
+            {
+                Console.WriteLine("Operetor '>'");
+                Console.Write("Set 1: ");
+                sample1.ShowSet();
+                Console.Write("Set 2: ");
+                sample2.ShowSet();
+                Console.Write("Result: ");
+                Console.WriteLine(sample1 > sample2);
+            }
             Console.ReadLine();
         }
     }
